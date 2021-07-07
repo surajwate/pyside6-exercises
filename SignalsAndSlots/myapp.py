d431cf68bd1ea375c39bd3227c8943b44b129a2f
Suraj Wate <suraj.wate@gmail.com>
2021-07-08 00:41:37 +0530

    passing data

--- SignalsAndSlots/myapp.py
+++ SignalsAndSlots/myapp.py
@@ -10,11 +10,15 @@ class MainWindow(QMainWindow):
         button = QPushButton("Press Me!")
         button.setCheckable(True)
         button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
 
         self.setCentralWidget(button)
 
     def the_button_was_clicked(self):
         print("Clicked!")
+
+    def the_button_was_toggled(self, checked):
+        print("Checked?", checked)
 
 
 app = QApplication([])
