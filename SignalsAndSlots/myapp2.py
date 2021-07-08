bc87454b0f64fb37a2ceb5cc0b9d3dbe4120cd47
Suraj Wate <suraj.wate@gmail.com>
2021-07-08 20:11:23 +0530

    added missing file

--- SignalsAndSlots/myapp2.py
+++ SignalsAndSlots/myapp2.py
@@ -1,52 +1,29 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
-from random import choice
 
-window_titles = [
-    'My App',
-    'My app',
-    'Still My App',
-    'Still My app',
-    'What on Earth',
-    'What on earth',
-    'This is surprising',
-    'This is Surprising',
-    'Something went wrong'
-]
+import sys
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.n_times_clicked = 0
+        self.setWindowTitle("My App")
 
-        self.setWindowTitle("Signals and Slots")
-
-        self.button = QPushButton("Press Me")
+        self.button = QPushButton("Press Me!")  # <1>
         self.button.clicked.connect(self.the_button_was_clicked)
 
-        self.windowTitleChanged.connect(self.the_window_title_changed)
-        """ Hook up our custom slot method the_window_title_changed to the windows .windowTitleChanged signal"""
-
+        # Set the central widget of the Window.
         self.setCentralWidget(self.button)
 
     def the_button_was_clicked(self):
-        """
-        """
-        print("Clicked")
-        new_window_title = choice(window_titles)
-        print(f"Setting title: {new_window_title}")
+        self.button.setText("You already clicked me.")  # <2>
+        self.button.setEnabled(False)  # <3>
 
-        self.setWindowTitle(new_window_title)
-
-    def the_window_title_changed(self, window_title):
-        print(f"Window title changed: {window_title}")
-
-        if window_title == "Something went wrong":
-            self.button.setDisabled(True)
+        # Also change the window title.
+        self.setWindowTitle("My Oneshot App")
 
 
-app = QApplication([])
+app = QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
---
+++ SignalsAndSlots/myapp3.py
@@ -0,0 +1,54 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from random import choice
+
+window_titles = [
+    'My App',
+    'My app',
+    'Still My App',
+    'Still My app',
+    'What on Earth',
+    'What on earth',
+    'This is surprising',
+    'This is Surprising',
+    'Something went wrong'
+]
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.n_times_clicked = 0
+
+        self.setWindowTitle("Signals and Slots")
+
+        self.button = QPushButton("Press Me")
+        self.button.clicked.connect(self.the_button_was_clicked)
+
+        self.windowTitleChanged.connect(self.the_window_title_changed)
+        """ Hook up our custom slot method the_window_title_changed to the windows .windowTitleChanged signal"""
+
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        """
+        """
+        print("Clicked")
+        new_window_title = choice(window_titles)
+        print(f"Setting title: {new_window_title}")
+
+        self.setWindowTitle(new_window_title)
+
+    def the_window_title_changed(self, window_title):
+        print(f"Window title changed: {window_title}")
+
+        if window_title == "Something went wrong":
+            self.button.setDisabled(True)
+
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
